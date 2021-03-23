62532c1219894629bae635e7cee7cc0e4260397e
Elisabeth Joan <Elisabeth.L.Joan@gmail.com>
2021-03-23 16:16:33 +1000

    React template initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LaserVisualiser from "./LaserVisualiser"
+import "./css/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <LaserVisualiser />
+    </div>
+  );
+}
+
+export default App;
